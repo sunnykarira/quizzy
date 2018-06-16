9b4d0a492aed9f317f1e070c19168880a1d737ce
Sunny Karira <sunny@sendinblue.com>
2018-06-16 18:56:36 +0530

    Add file reading

--- flags.go
+++ flags.go
@@ -1,1 +1,14 @@
 package main
+
+import "flag"
+
+type flags struct {
+	fileName string
+}
+
+func getFlags() *flags {
+	flg := &flags{}
+	flag.StringVar(&flg.fileName, "filename", "problems.csv", "file name to parse questions")
+	flag.Parse()
+	return flg
+}
--- main.go
+++ main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	// Get Flags
+	flg := getFlags()
+	//Read the file
+	f, err := ioutil.ReadFile(flg.fileName)
+	if err != nil {
+		errorHandler(err)
+	}
+	fmt.Print(string(f))
+}
+
+func errorHandler(err error) {
+	fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	os.Exit(1)
+}
